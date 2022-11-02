ba12eaad353142b54709d96f2faca33dd5c44e8b
Parzival8971 <esh8971@gmail.com>
2022-11-02 22:55:43 +0900

    SigininCss을 만든다

--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -5,30 +5,46 @@ const Signin = () => {
   return (
     <Row align='middle' className={styles.signin_row}>
       <Col span={24}>
-        <Row>
-          <Col span={12}></Col>
+        <Row className={styles.signin_contents}>
           <Col span={12}>
-            <div>My Books</div>
-            <div>Please Note Your Opinion</div>
-            <div />
-            <div>
-              Email<span> *</span>
+            <img
+              className={styles.signin_bg}
+              alt='Signin'
+              src='/bg_signin.png'
+            />
+          </Col>
+          <Col span={12}>
+            <div className={styles.signin_title}>My Books</div>
+            <div className={styles.signin_subtitle}>
+              Please Note Your Opinion
             </div>
-            <div>
-              <Input placeholder='Email' autoComplete='email' name='email' />
+            <div className={styles.signin_underline} />
+            <div className={styles.email_title}>
+              Email<span className={styles.required}> *</span>
             </div>
-            <div>
-              Password<span> *</span>
-            </div>
-            <div>
+            <div className={styles.input_area}>
               <Input
+                className={styles.input}
+                placeholder='Email'
+                autoComplete='email'
+                name='email'
+              />
+            </div>
+            <div className={styles.password_title}>
+              Password<span className={styles.required}> *</span>
+            </div>
+            <div className={styles.input_area}>
+              <Input
+                className={styles.input}
                 type='password'
                 autoComplete='current-password'
                 name='email'
               />
             </div>
-            <div>
-              <Button size='large'>Sign In</Button>
+            <div className={styles.button_area}>
+              <Button className={styles.button} size='large'>
+                Sign In
+              </Button>
             </div>
           </Col>
         </Row>
